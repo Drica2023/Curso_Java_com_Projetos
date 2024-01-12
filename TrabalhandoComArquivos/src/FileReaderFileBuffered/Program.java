98aded4573f6592970e044ed69d914e057d36d1a
Drica2023 <adrianamedeiros.dev@outlook.com>
2024-01-12 16:00:44 -0300

    Trabalhando com arquivos - forma simplificada com Bloco try with resources

--- TrabalhandoComArquivos/src/FileReaderFileBuffered/Program.java
+++ TrabalhandoComArquivos/src/FileReaderFileBuffered/Program.java
@@ -5,43 +5,28 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Program {
-	
-	public static void main (String []args) {
-		
+
+	public static void main(String[] args) {
+
 		String path = "C:\\Users\\Medeiros\\Documents\\MySql.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
-		try {
-			
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+		
+
 			String line = br.readLine();
 			while (line != null) {
 				System.out.println(line);
 				line = br.readLine();
 			}
-			
-		}catch(IOException e) {
+
+		} catch (IOException e) {
 			System.out.println("arquivo com erro" + e.getMessage());
-			
-		}finally {
-			try {
-				fr.close();
-				
-				br.close();
-				
-				
-			}catch(IOException e){
-				e.printStackTrace();
-				
-				
-			}
-			
-		}
+
+		} 
+
 		
-		
-		
+
 	}
 
 }
